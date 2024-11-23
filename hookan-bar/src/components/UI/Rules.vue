876b5ed5b7e94cb5081a9d5416396ba8a4d24f44
PDergunova <PDergunova@croc.ru>
2024-11-24 01:19:20 +0800

    Правки кода

--- hookan-bar/src/components/UI/Rules.vue
+++ hookan-bar/src/components/UI/Rules.vue
@@ -1,118 +1,105 @@
 <template>
     <div class="rules">
-      <div class="flex column items-center">
-        <img src="../../assets/18plus.svg" width="40px" height="40px">
-        <p class="text-center">
-          Не допускаются лица
-          <br> моложе <a class="text-red-12">18 лет</a>
-        </p>
-      </div>
-      <div class="flex column items-center">
-        <img src="../../assets/shirt.svg" width="40px" height="40px">
-        <p class="text-center">
-          Gorgona Lounge<br>
-          <a class="text-red-12">не несет ответственность</a><br>
-          за личные вещи гостей
-        </p>
-      </div>
-      <div class="flex justify-center">
-        <div class="bordered q-mx-lg">
-          <p class="text-center text-red-12 q-mb-xs q-mt-sm">ЗАПРЕЩАЕТСЯ:</p>
-          <ul class="q-mx-lg list-none text-center">
-            <li class="q-my-sm">Курение сигарет и электронных устройств</li>
-            <hr class="thin-hr">
-            <li class="q-my-sm">Находиться в состоянии сильного алкогольного опьянения и под воздействием запрещенных
-              веществ</li>
-          </ul>
-        </div>
-      </div>
-      <div class="q-my-md q-mx-md">
-        <p class="text-center"><a class="text-red-12">При порче имущества</a> заведения гость оплачивает стоимость
-          имущества согласно конфликт-меню</p>
-        <p class="text-center"><a class="text-red-12">Заведение не несет никакой ответственности </a> за повреждения,
-          травмы, ущерб, смертельные или другие случаи возникшие во время посещения заведения</p>
-      </div>
-      <div class="flex justify-center">
-        <div class="bordered q-mx-lg">
-          <div class="flex column items-center">
-            <img src="../../assets/mark.svg" class="q-mt-sm" width="40px" height="40px">
-            <p class="text-center text-red-12">
-              За нарушение правил администрация вправе отказать Вам <br> в обслуживании и дальнейшем посещении
+        <div class="flex column items-center">
+            <img src="../../assets/18plus.svg" width="40px" height="40px">
+            <p class="text-center">
+                Не допускаются лица
+                <br> моложе <a class="text-red-12">18 лет</a>
             </p>
-          </div>
         </div>
-      </div>
-  
-      <div class="text-center q-gutter-sm q-my-lg rules-smoke">
-        <h5 class="q-mb-lg">Правила курения</h5>
-        <div class="row">
-          <div class="bordered-white">1—3<br>PEOPLE</div>
-          <img src="../../assets/hookan.svg" width="50px" height="50px">
+        <div class="flex column items-center">
+            <img src="../../assets/shirt.svg" width="40px" height="40px">
+            <p class="text-center">
+                Gorgona Lounge<br>
+                <a class="text-red-12">не несет ответственность</a><br>
+                за личные вещи гостей
+            </p>
         </div>
-        <div class="row">
-          <div class="bordered-white">4—6<br>PEOPLE</div>
-          <img src="../../assets/hookan.svg" width="50px" height="50px">
-          <img src="../../assets/hookan.svg" width="50px" height="50px">
+        <div class="flex justify-center">
+            <div class="bordered q-mx-lg">
+                <p class="text-center text-red-12 q-mb-xs q-mt-sm">ЗАПРЕЩАЕТСЯ:</p>
+                <ul class="q-mx-lg list-none text-center">
+                    <li class="q-my-sm">Курение сигарет и электронных устройств</li>
+                    <hr class="thin-hr">
+                    <li class="q-my-sm">Находиться в состоянии сильного алкогольного опьянения и под воздействием
+                        запрещенных
+                        веществ</li>
+                </ul>
+            </div>
         </div>
-        <div class="row">
-          <div class="bordered-white">7—9<br>PEOPLE</div>
-          <img src="../../assets/hookan.svg" width="50px" height="50px">
-          <img src="../../assets/hookan.svg" width="50px" height="50px">
-          <img src="../../assets/hookan.svg" width="50px" height="50px">
+        <div class="q-my-md q-mx-md">
+            <p class="text-center"><a class="text-red-12">При порче имущества</a> заведения гость оплачивает стоимость
+                имущества согласно конфликт-меню</p>
+            <p class="text-center"><a class="text-red-12">Заведение не несет никакой ответственности </a> за
+                повреждения,
+                травмы, ущерб, смертельные или другие случаи возникшие во время посещения заведения</p>
         </div>
-        <div class="row">
-          <div class="bordered-white">10—12<br>PEOPLE</div>
-          <img src="../../assets/hookan.svg" width="50px" height="50px">
-          <img src="../../assets/hookan.svg" width="50px" height="50px">
-          <img src="../../assets/hookan.svg" width="50px" height="50px">
-          <img src="../../assets/hookan.svg" width="50px" height="50px">
+        <div class="flex justify-center">
+            <div class="bordered q-mx-lg">
+                <div class="flex column items-center">
+                    <img src="../../assets/mark.svg" class="q-mt-sm" width="40px" height="40px">
+                    <p class="text-center text-red-12 q-mx-sm">
+                        За нарушение правил администрация вправе отказать Вам <br> в обслуживании и дальнейшем посещении
+                    </p>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </template>
-  
-  <script setup lang="ts">
 
-  </script>
-  
-  <style lang="scss" scoped>
-  .rules {
+        <div class="text-center q-gutter-sm q-my-lg rules-smoke">
+            <h5 class="q-mb-lg">Правила курения</h5>
+            <div v-for="(rule, index) in rules" :key="index" class="row">
+                <div class="bordered-white">
+                    {{ rule.people }}<br>PEOPLE
+                </div>
+                <img v-for="i in rule.hookahs" :key="i" src="../../assets/hookan.svg" width="50px" height="50px">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const rules = [
+    { people: '1—3', hookahs: 1 },
+    { people: '4—6', hookahs: 2 },
+    { people: '7—9', hookahs: 3 },
+    { people: '10—12', hookahs: 4 },
+];
+</script>
+
+<style lang="scss" scoped>
+.rules {
     color: white;
     font-size: 17px;
     word-break: break-word;
     list-style: none;
-  }
-  
-  ul {
+}
+
+ul {
     list-style-type: none;
-  }
-  
-  .thin-hr {
+}
+
+.thin-hr {
     border: 1px solid #ff5252;
     border-width: 1px 0 0 0;
     width: 270px;
     margin: auto;
-  }
-  
-  .bordered {
+}
+
+.bordered {
     border: 1px solid #ff5252;
     width: 100%;
     max-width: 600px;
-  }
-  
-  .bordered-red {
-    border: 1px solid #ff5252;
-  }
-  
-  .bordered-white {
+}
+
+.bordered-white {
     border: 1px solid #ffffff;
     width: 80px;
     margin-right: 20px;
-  }
-  
-  .rules-smoke {
+}
+
+.rules-smoke {
     width: 100%;
     max-width: 310px;
     margin: 0 auto;
-  }
-  </style>
+}
+</style>
